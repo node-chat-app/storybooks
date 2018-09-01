41f2406ec0d3e790c6c6ba361923d2cdff9fdfde
george <nwagbarag@gmail.com>
2018-09-01 12:06:44 +0100

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports ={
+mongoURI:'mongodb://AgbaraNwa:george03@ds239682.mlab.com:39682/storybooks-dev',
+googleClientID:'871607645561-85auoo48aqc3ctuorsq58887h3muueht.apps.googleusercontent.com',
+googleClientSecret:'WNKc7KcE0wbTGr6Y9HC9B6DD'
+};
