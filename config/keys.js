0b0b5ccf0695a983180db11f239b306e9198e667
george <nwagbarag@gmail.com>
2018-09-30 20:50:23 +0100

    make it better

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports ={
-mongoURI:'mongodb://george:george03@ds239682.mlab.com:39682/storybooks-dev',
-googleClientID:'871607645561-85auoo48aqc3ctuorsq58887h3muueht.apps.googleusercontent.com',
-googleClientSecret:'WNKc7KcE0wbTGr6Y9HC9B6DD'
-};
+if(process.env.NODE_ENV ==='production'){
+  module.exports = require('./keys_prod');
+} else{
+  module.exports = require ('./keys_dev');
+}
